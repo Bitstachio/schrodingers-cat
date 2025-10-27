d58126ad1d8fdf5d2d45478bfc66dae0ae0b28a8
BarbodH <habibi.k.barbod@gmail.com>
2025-10-27 04:35:41 -0400

    Implement console trigger script & prefabs

---
+++ Assets/Features/Console/Scripts/ConsoleTrigger.cs
@@ -0,0 +1,52 @@
+using Features.Console.Interfaces;
+using Features.Indicator.Scripts;
+using UnityEngine;
+using VContainer;
+
+namespace Features.Console.Scripts
+{
+    public class ConsoleTrigger : MonoBehaviour
+    {
+        //===== Inspector Fields =====
+
+        [SerializeField] private IndicatorImpl indicator;
+
+        //===== Internal Fields =====
+
+        private bool _playerInRange;
+
+        //===== Dependency Injection =====
+
+        private IConsoleService _consoleService;
+
+        [Inject]
+        public void Construct(IConsoleService consoleService)
+        {
+            _consoleService = consoleService;
+        }
+
+        //===== Lifecycle =====
+
+        private void Update()
+        {
+            // TODO: Consider refactoring input handling to use input action
+            if (_playerInRange && Input.GetKeyDown(KeyCode.E)) _consoleService.Open();
+        }
+
+        //===== Trigger Events =====
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.CompareTag("Player")) return;
+            _playerInRange = true;
+            indicator.Show();
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (!other.CompareTag("Player")) return;
+            _playerInRange = false;
+            indicator.Hide();
+        }
+    }
+}
---
+++ Assets/Features/Indicator/Interfaces/IIndicator.cs
@@ -0,0 +1,8 @@
+namespace Features.Indicator.Interfaces
+{
+    public interface IIndicator
+    {
+        void Show();
+        void Hide();
+    }
+}
