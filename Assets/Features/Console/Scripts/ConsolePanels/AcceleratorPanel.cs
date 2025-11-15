a8e5c6b437d3fbc08a30ecc6a0c26cfea3a79d73
BarbodH <habibi.k.barbod@gmail.com>
2025-11-15 04:13:19 -0500

    Create abstract base class & exception for console panels

--- Assets/Features/Console/Scripts/ConsolePanels/AcceleratorPanel.cs
+++ Assets/Features/Console/Scripts/ConsolePanels/AcceleratorPanel.cs
@@ -1,32 +1,6 @@
-using Features.Console.Exceptions;
-using Features.Console.Interfaces;
-using UnityEngine;
-
 namespace Features.Console.Scripts.ConsolePanels
 {
-    public class AcceleratorPanel : MonoBehaviour, IConsolePanel
+    public class AcceleratorPanel : BaseConsolePanel
     {
-        [SerializeField] private int consoleId;
-
-        public int Id => consoleId;
-
-        //===== Internal Fields =====
-
-        private int? _consoleId; 
-
-        public void Show(int consoleId)
-        {
-            _consoleId = consoleId;
-            gameObject.SetActive(true);
-            // TODO: Load data corresponding to console ID
-        }
-
-        public void Hide()
-        {
-            // TODO: Determine if this is the appropriate exception class in this scope 
-            if (!_consoleId.HasValue) throw new ConsoleNotOpenException();
-            gameObject.SetActive(false);
-            // TODO: Save data corresponding to console ID 
-        }
     }
 }
