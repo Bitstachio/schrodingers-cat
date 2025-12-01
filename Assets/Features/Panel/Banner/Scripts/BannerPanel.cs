f1c37ff69f862d085e907ed3b6dfa7a33fbbf600
BarbodH <habibi.k.barbod@gmail.com>
2025-12-01 16:19:32 -0500

    Set up banner dynamic content using scriptable object

--- Assets/Features/Panel/Banner/Scripts/BannerPanel.cs
+++ Assets/Features/Panel/Banner/Scripts/BannerPanel.cs
@@ -1,19 +1,24 @@
 using Features.Panel.Common.Exceptions;
+using TMPro;
 using UnityEngine;
 
 namespace Features.Panel.Banner.Scripts
 {
     public class BannerPanel : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI textComponent;
+        
         public void Show(BannerContent banner)
         {
             if (gameObject.activeSelf) throw new PanelAlreadyOpenException();
+            textComponent.text = banner.Text;
             gameObject.SetActive(true);
         }
 
         public void Hide()
         {
             if (!gameObject.activeSelf) throw new PanelNotOpenException();
+            textComponent.text = string.Empty;
             gameObject.SetActive(false);
         }
     }
