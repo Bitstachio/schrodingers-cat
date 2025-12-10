779ae26e99ef65f6114102362b1db482ff8c167d
BarbodH <habibi.k.barbod@gmail.com>
2025-12-10 17:42:09 -0500

    Remove unused static panel service + polish installer

--- Assets/Installers/RootLifetimeScope.cs
+++ Assets/Installers/RootLifetimeScope.cs
@@ -1,9 +1,6 @@
 using System.Linq;
 using Features.Panel.Common.Interfaces;
 using Features.Panel.Common.Services;
-using Features.Panel.StaticPanel.Interfaces;
-using Features.Panel.StaticPanel.Scripts;
-using Features.Panel.StaticPanel.Services;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -20,24 +17,8 @@ namespace Installers
 
         protected override void Configure(IContainerBuilder builder)
         {
-            //===== Panel Service =====
+            //===== Global Interface Injection =====
 
-            builder.Register<IPanelService, StaticPanelService>(Lifetime.Singleton);
-
-            builder.RegisterBuildCallback(container =>
-            {
-                var triggers = FindObjectsByType<StaticPanelInteractable>(FindObjectsSortMode.None);
-                foreach (var trigger in triggers) container.Inject(trigger);
-            });
-            builder.RegisterComponentInHierarchy<StaticPanelDispatcher>();
-
-            //===== Panel Service =====
-
-            builder.Register(typeof(PanelService<>), Lifetime.Singleton)
-                .As(typeof(IPanelService<>))
-                .AsSelf();
-
-            // Inject into all interface inheritors
             builder.RegisterBuildCallback(container =>
             {
                 foreach (var behaviour in FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None))
@@ -49,6 +30,12 @@ namespace Installers
                     }
                 }
             });
+
+            //===== Services =====
+
+            builder.Register(typeof(PanelService<>), Lifetime.Singleton)
+                .As(typeof(IPanelService<>))
+                .AsSelf();
         }
     }
 }
