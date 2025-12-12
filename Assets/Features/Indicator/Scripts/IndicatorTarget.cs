0a9cdfd3a57e1fd1d45ea54f767bca322bc4a37b
BarbodH <habibi.k.barbod@gmail.com>
2025-12-12 04:36:00 -0500

    Add exceptions to enforce indicator preconditions for collider

--- Assets/Features/Indicator/Scripts/IndicatorTarget.cs
+++ Assets/Features/Indicator/Scripts/IndicatorTarget.cs
@@ -1,3 +1,4 @@
+using Shared.Exceptions;
 using Shared.Scripts;
 using UnityEngine;
 
@@ -7,6 +8,19 @@ namespace Features.Indicator.Scripts
     public class IndicatorTarget : MonoBehaviour
     {
         [SerializeField] private Indicator indicator;
+
+        private const int NumColliders = 1;
+
+        //===== Lifecycle =====
+
+        private void Awake()
+        {
+            // Validate that exactly 1 circle collider exists and is set as a trigger
+            var colliders = GetComponents<CircleCollider2D>();
+            if (colliders.Length > NumColliders)
+                throw new TooManyComponentsException(typeof(CircleCollider2D), NumColliders, colliders.Length);
+            if (!colliders[0].isTrigger) throw new ColliderNotTriggerException();
+        }
 
         //===== Trigger Events =====
 
