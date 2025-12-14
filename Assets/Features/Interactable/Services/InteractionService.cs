eb3d61efdd622026a6253ff3749267f8e7a91559
BarbodH <habibi.k.barbod@gmail.com>
2025-12-14 02:07:10 -0500

    Define & register interactable service

---
+++ Assets/Features/Interactable/Services/InteractionService.cs
@@ -0,0 +1,20 @@
+using Features.Interactable.Interfaces;
+using Shared.Contracts;
+using VContainer;
+
+namespace Features.Interactable.Services
+{
+    public class InteractionService<T> : IInteractableService<T>
+    {
+        //===== Dependency Injection =====
+
+        private IInteractableEventPublisher<T> _eventPublisher;
+
+        [Inject]
+        public void Construct(IInteractableEventPublisher<T> eventPublisher) => _eventPublisher = eventPublisher;
+
+        //===== Interface Implementation =====
+
+        public void Apply(T content) => _eventPublisher.Invoke(content);
+    }
+}
