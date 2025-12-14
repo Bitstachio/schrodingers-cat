888e9e7bd39f1c1908672fe3ac0e6a667093538a
BarbodH <habibi.k.barbod@gmail.com>
2025-12-14 05:41:11 -0500

    Incorporate InteractableBehaviour into dialogue & static panel

--- Assets/Features/Interactable/Interfaces/IInteractable.cs
+++ Assets/Features/Interactable/Interfaces/IInteractable.cs
@@ -1,5 +1,11 @@
 namespace Features.Interactable.Interfaces
 {
+    /// <summary>
+    /// Marker interface for classes that represent interactable objects.
+    /// 
+    /// This interface has no methods or properties. It is used by the global dependency injector to identify  classes
+    /// that require event-related dependencies to be injected. Refer to <see cref="Installers.GlobalInjectionConfig"/>.
+    /// </summary>
     public interface IInteractable
     {
     }
--- Assets/Features/Interactable/Scripts/StaticPanelInteractable.cs
+++ Assets/Features/Interactable/Scripts/StaticPanelInteractable.cs
@@ -1,24 +1,12 @@
-using Features.Interactable.Interfaces;
 using Shared.EventBus.Structs;
 using UnityEngine;
-using VContainer;
 
 namespace Features.Interactable.Scripts
 {
-    public class StaticPanelInteractable : MonoBehaviour, IInteractable
+    public class StaticPanelInteractable : InteractableBehaviour<StaticPanelInteractionEventArgs>
     {
         [SerializeField] private int panelId;
 
-        //===== Dependency Injection =====
-
-        private IInteractableService<StaticPanelInteractionEventArgs> _interactableService;
-
-        [Inject]
-        public void Construct(IInteractableService<StaticPanelInteractionEventArgs> interactableService) =>
-            _interactableService = interactableService;
-
-        //===== Interface Implementation =====
-
-        public void Interact() => _interactableService.Apply(new StaticPanelInteractionEventArgs(panelId));
+        protected override void Interact() => InteractableService.Apply(new StaticPanelInteractionEventArgs(panelId));
     }
 }
