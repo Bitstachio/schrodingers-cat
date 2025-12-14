7cb737a7bbdbedddda852523d449f139c3b5bc0e
BarbodH <habibi.k.barbod@gmail.com>
2025-12-14 07:35:13 -0500

    Incorporate ID in puzzle

--- Assets/Shared/EventBus/Structs/PuzzleAttemptEventArgs.cs
+++ Assets/Shared/EventBus/Structs/PuzzleAttemptEventArgs.cs
@@ -2,8 +2,13 @@ namespace Shared.EventBus.Structs
 {
     public readonly struct PuzzleAttemptEventArgs
     {
+        public int Id { get; }
         public bool Result { get; }
 
-        public PuzzleAttemptEventArgs(bool result) => Result = result;
+        public PuzzleAttemptEventArgs(int id, bool result)
+        {
+            Id = id;
+            Result = result;
+        }
     }
 }
