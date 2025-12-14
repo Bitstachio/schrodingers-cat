a5742b994a3f4b1a8dac1a6da68dc599ec121e51
BarbodH <habibi.k.barbod@gmail.com>
2025-12-14 05:16:00 -0500

    Incorporate generic event listener

--- Assets/Features/Panel/Scripts/Listeners/StaticPanelListener.cs
+++ Assets/Features/Panel/Scripts/Listeners/StaticPanelListener.cs
@@ -1,25 +1,15 @@
 using System.Linq;
 using Features.Panel.Exceptions;
 using Features.Panel.Scripts.Panels;
-using Shared.EventBus.Interfaces;
+using Shared.EventBus.Implementation;
 using Shared.EventBus.Structs;
 using UnityEngine;
-using VContainer;
 
 namespace Features.Panel.Scripts.Listeners
 {
-    public class StaticPanelListener : MonoBehaviour, IEventListener
+    public class StaticPanelListener : EventListener<StaticPanelInteractionEventArgs>
     {
         private StaticPanel[] _panels;
-
-        //===== Dependency Injection =====
-
-        private IEvent<StaticPanelInteractionEventArgs> _event;
-
-        [Inject]
-        public void Construct(IEvent<StaticPanelInteractionEventArgs> @event) => _event = @event;
-
-        //===== Lifecycle =====
 
         private void Awake()
         {
@@ -28,15 +18,7 @@ namespace Features.Panel.Scripts.Listeners
                 .ToArray();
         }
 
-        // Handle subscription to panel service
-
-        private void OnEnable() => _event.Invoked += OnPanelOpened;
-
-        private void OnDisable() => _event.Invoked -= OnPanelOpened;
-
-        //===== Event Handlers =====
-
-        private void OnPanelOpened(StaticPanelInteractionEventArgs e)
+        protected override void OnInvoked(StaticPanelInteractionEventArgs e)
         {
             var panel = _panels.FirstOrDefault(p => p.Id == e.PanelId) ?? throw new PanelIdNotFound(e.PanelId);
             panel.Show(e.PanelId);
