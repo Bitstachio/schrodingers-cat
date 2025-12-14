862c7c818a3f35240a24967770164840790b5169
BarbodH <habibi.k.barbod@gmail.com>
2025-12-14 09:34:01 -0500

    Invoke task list completion event

--- Assets/Features/Task/Scripts/TaskPuzzleListener.cs
+++ Assets/Features/Task/Scripts/TaskPuzzleListener.cs
@@ -1,8 +1,10 @@
 using System.Linq;
+using Features.Task.Interfaces;
 using Shared.EventBus.Implementation;
 using Shared.EventBus.Structs;
 using Shared.ScriptableObjects.Task;
 using UnityEngine;
+using VContainer;
 
 namespace Features.Task.Scripts
 {
@@ -10,12 +12,18 @@ namespace Features.Task.Scripts
     {
         [SerializeField] private TaskList taskList;
 
+        private ITaskService _taskService;
+
+        [Inject]
+        public void Construct(ITaskService taskService) => _taskService = taskService;
+
         // TODO: This is a temporary solution; investigate a better approach to prevent SO from persisting progress
         private void Awake() => taskList.ResetProgress();
 
         protected override void OnInvoked(PuzzleAttemptEventArgs e)
         {
             if (e.Result) taskList.Tasks.FirstOrDefault(t => t.Id == e.Id)?.MakeProgress();
+            if (taskList.IsComplete()) _taskService.Complete(e.Id);
         }
     }
 }
