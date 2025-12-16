bb067e19ff366cb7517d49ea904400311fd479a3
BarbodH <habibi.k.barbod@gmail.com>
2025-12-16 12:53:09 -0500

    Make cats face player on x-axis during encounter

--- Assets/Features/NPC/Cats/Shared/Scripts/CatEncounter.cs
+++ Assets/Features/NPC/Cats/Shared/Scripts/CatEncounter.cs
@@ -10,17 +10,41 @@ namespace Features.NPC.Cats.Shared.Scripts
         private static readonly int PlayerInRange = Animator.StringToHash("PlayerInRange");
 
         private Animator _animator;
+        private Transform _player;
 
         private void Awake() => _animator = GetComponent<Animator>();
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.CompareTag(Tags.Player)) _animator.SetBool(PlayerInRange, true);
+            if (!other.CompareTag(Tags.Player)) return;
+
+            _player = other.transform;
+            _animator.SetBool(PlayerInRange, true);
+
+            FacePlayer();
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
-            if (other.CompareTag(Tags.Player)) _animator.SetBool(PlayerInRange, false);
+            if (!other.CompareTag(Tags.Player)) return;
+
+            _animator.SetBool(PlayerInRange, false);
+            _player = null;
+        }
+
+        private void Update()
+        {
+            if (_player != null) FacePlayer();
+        }
+
+        private void FacePlayer()
+        {
+            var direction = _player.position.x - transform.position.x;
+            if (Mathf.Approximately(direction, 0f)) return;
+
+            var scale = transform.localScale;
+            scale.x = Mathf.Sign(direction) * Mathf.Abs(scale.x);
+            transform.localScale = scale;
         }
     }
 }
