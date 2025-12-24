2c8e5346aec6212461e8b4aaa9401f349587860d
BarbodH <habibi.k.barbod@gmail.com>
2025-12-24 17:35:18 -0500

    Set up main menu back button

--- Assets/Features/MainMenu/Scripts/MainMenu.cs
+++ Assets/Features/MainMenu/Scripts/MainMenu.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 namespace Features.MainMenu.Scripts
 {
+    // TODO: This works, but I'm not sure if it's best practice; especially for panels game object array
     public class MainMenu : MonoBehaviour
     {
-        
+        [SerializeField] private GameObject backButton;
+        [SerializeField] private GameObject[] pages;
+
+        public void BackToMain()
+        {
+            backButton.SetActive(false);
+            // TODO: I don't like using a magic string here
+            foreach (var page in pages) page.SetActive(page.name == "Main");
+        }
     }
 }
